4bca78e901957fbb62d38437446a2e5eb607f0a7
mjschwarz <mattschwarz5@gmail.com>
2021-11-21 22:48:32 -0500

    Redesigned the volatility algorithm and switched data source to Yahoo Finance.

--- README.md
+++ README.md
@@ -2,28 +2,32 @@
 
 ### Description
 
-This program models the returns of stocks versus the returns of the S&P 500. 
-Please note that this analysis should not be used for investment purposes.
+This program analyzes the volatility of stocks versus that of the S&P 500. 
+
+`Note:` This analysis should not be used for investment purposes.
 
 ### Functionality
 
-
-- Returns can be calculated and visualized on a Yearly, Monthly, Weekly, or Daily basis.
-- To analyze a stock, place it into the directory, *stock_data*.
-- Additional stocks to analyze can be found in the directory, *extra_stock_data*.
-- Alternatively, data can be downloaded from [Nasdaq](https://www.nasdaq.com/market-activity/quotes/historical) in csv format (select MAX timeframe).
+- Enter the stock tickers that you wish to analyze separated by whitespace.
+- Enter the year, day, and month from which to begin analysis as prompted.
+- Enter the year, day, and month at which to end analysis as prompted.
+- The annual, monthly, weekly, and daily volatility of the S&P 500 and each requested stock 
+will be printed to the console.
+- A plot of the daily volatility of the S&P 500 and all requested stock will be displayed.
 
 ### Imports
 
-This program uses the Pandas library to manipulate large datasets. 
-Additionally, it uses the Matplotlib library to visualize the analyzed data.
-It also uses the glob module to find all files within a directory.
+This program uses the *pandas* library to manipulate large datasets and the *pandas-datareader* module to
+parse stock data from Yahoo Finance. It also uses the *datetime* module to set the date range to analyze.
+Additionally, it uses the Matplotlib library to visualize the analyzed data. Finally, it uses the *math* module to
+perform some mathematical calculations.
 
-### Examples
+### Example Output
 
-![](images/xom_example.png?raw=true "Exxon Mobile")
-![](images/gm_tsla_example.png?raw=true "GM and Tesla")
+![](img/aapl_tsla_example.png?raw=true "Apple and Tesla")
+![](img/msft_example.png?raw=true "Microsoft")
+![](img/console_output.png?raw=true "Console Output")
 
 ### GitHub
 
-The GitHub repository for this project can be found [here](https://github.com/mjschwarz/BasicStockAnalysis.git).
+The GitHub repository for this project can be found [here](https://github.com/mjschwarz/StockVolatility.git).
--- main.py
+++ main.py
@@ -1,77 +1,116 @@
-import pandas as pd
+import math
+import pandas_datareader.data as web
 import matplotlib.pyplot as plt
-import glob
+import matplotlib.ticker as mtick
+import datetime as dt
 
 
-# ----------
-# CONSTANTS
-# ----------
-SP_500 = 'stock_data/voo_historical_data.csv'
-returns_interval = input('Enter the time interval at which to calculate returns (Yearly, Monthly, Weekly, Daily): ')
+# -----------------------
+# CONSTANTS AND VARIABLES
+# -----------------------
+TRADING_DAYS_YEAR = 252
+TRADING_DAYS_MONTH = 21
+TRADING_DAYS_WEEK = 4.83
+
+SP_500 = 'VOO'
+
+tickers = input('Enter the STOCK TICKER(S) separated by spaces: ').upper().split()
+print()
+
+START = dt.datetime(int(input('Enter the STARTING YEAR: ')),
+                    int(input('Enter the STARTING DAY: ')),
+                    int(input('Enter the STARTING MONTH: ')))
+print()
+
+END = dt.datetime(int(input('Enter the ENDING YEAR: ')),
+                  int(input('Enter the ENDING DAY: ')),
+                  int(input('Enter the ENDING MONTH: ')))
+print()
 
 
 # -----------
 # PLOT SETUP
 # -----------
 plt.style.use('seaborn-darkgrid')
-plt.title('Stock Performance vs. S&P 500 (' + returns_interval + ')')
-plt.xlabel('Time')
-plt.ylabel('Returns')
+plt.title('Stock Volatility vs. S&P 500')
+plt.ylabel('Volatility')
+plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1, decimals=None, symbol='%', is_latex=False))
 
 
 # ---------------
 # HELPER METHODS
 # ---------------
-def get_interval_returns(daily_returns, interval):
-    """Calculates the stock returns at the given interval and plots the result.
-
-    :param daily_returns: Pandas DataFrame
-                        index: Datetime object representing the current date
-                        column: daily stock returns data
-    :param interval: str
-                    time interval at which the calculate the stock returns (can be Y, M, W, D)
+def calc_volatility(d_pct_change, tkr):
     """
+    Calculates and prints to the console the volatility of a stock
+    at annual, monthly, weekly, and daily intervals.
+    :param d_pct_change: daily percentage change in adjusted closing price
+    :param tkr: ticker
+    """
+    annual_volatility = d_pct_change.std() * math.sqrt(TRADING_DAYS_YEAR)
+    monthly_volatility = d_pct_change.std() * math.sqrt(TRADING_DAYS_MONTH)
+    weekly_volatility = d_pct_change.std() * math.sqrt(TRADING_DAYS_WEEK)
+    daily_volatility = d_pct_change.std()
 
-    # calculate the weekly average return
-    stock_returns_interval = daily_returns.resample(interval).mean()
-
-    # plot monthly stock returns
-    plt.plot(stock_returns_interval.index, stock_returns_interval,
-             label='S&P 500' if file == SP_500 else file.split("stock_data/")[1].split("_")[0],
-             alpha=0.8 if file == SP_500 else 0.6,
-             zorder=10 if file == SP_500 else 0)
-
-
-# ------------------
-# DATA MANIPULATION
-# ------------------
-for file in glob.glob('stock_data/*'):
-    # read in data from csv file
-    stock_price_daily = pd.read_csv(file,
-                                    usecols=['Date', 'Close/Last'],
-                                    parse_dates=['Date'],
-                                    infer_datetime_format=True,
-                                    index_col=['Date'])
-    # remove $ and commas from stock prices
-    stock_price_daily['Close/Last'] = stock_price_daily['Close/Last'].astype(str).map(lambda x: x.replace('$', '').replace(',', ''))
-    # calculate the percent change in stock price from previous day (daily return)
-    stock_returns_daily = stock_price_daily['Close/Last'].astype(float).pct_change()
-
-    # determine returns time interval
-    if returns_interval == 'Yearly':
-        get_interval_returns(stock_returns_daily, 'Y')
-    elif returns_interval == 'Weekly':
-        get_interval_returns(stock_returns_daily, 'W')
-    elif returns_interval == 'Daily':
-        get_interval_returns(stock_returns_daily, 'D')
+    if tkr == SP_500:
+        print('S&P 500 Volatility')
     else:
-        get_interval_returns(stock_returns_daily, 'M')
+        print(f'{tkr} Volatility')
+    print(f'Annual: {annual_volatility * 100:.2f}%')
+    print(f'Monthly: {monthly_volatility * 100:.2f}%')
+    print(f'Weekly: {weekly_volatility * 100:.2f}%')
+    print(f'Daily: {daily_volatility * 100:.2f}%')
+    print()
+
+
+def get_data(tkr):
+    """
+    Gets the adjusted closing price data of a stock.
+    Data retrieved from Yahoo Finance.
+    :param tkr: ticker
+    :return: daily adjusted closing price
+    """
+    daily_adj_close = web.DataReader(tkr, 'yahoo', start=START, end=END)['Adj Close']
+    return daily_adj_close
+
+
+def calc_pct_change(d_adj_close):
+    """
+    Calculates the daily percentage change in adjusted closing price.
+    :param d_adj_close: daily adjusted closing price
+    :return: daily percentage change in adjusted closing price
+    """
+    daily_pct_change = d_adj_close.pct_change()
+    return daily_pct_change
+
+
+def plot_data(d_pct_change, tkr):
+    """
+    Plots the daily volatility data from the parametrized ticker.
+    :param d_pct_change: daily volatility data
+    :param tkr: ticker
+    """
+    if tkr == SP_500:
+        d_pct_change.plot(label='S&P 500', zorder=10)
+    else:
+        d_pct_change.plot(label=tkr, alpha=0.8)
+
+
+# --------------------------------
+# DATA COLLECTION AND MANIPULATION
+# --------------------------------
+SP_500_data = calc_pct_change(get_data(SP_500))
+plot_data(SP_500_data, SP_500)
+calc_volatility(SP_500_data, SP_500)
+
+for ticker in tickers:
+    ticker_data = calc_pct_change(get_data(ticker))
+    plot_data(ticker_data, ticker)
+    calc_volatility(ticker_data, ticker)
+
 
 # -------------
 # DISPLAY PLOT
 # -------------
 plt.legend()
 plt.show()
-
-
-
